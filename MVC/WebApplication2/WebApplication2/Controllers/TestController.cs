9db9427c079e942ef35535b0d0f04689c1f17861
Begin  Again <44010251+M1114378154@users.noreply.github.com>
2018-12-07 08:10:40 +0800

    1207

---
+++ MVC/WebApplication2/WebApplication2/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello! MVC~";
+        }
+        public Customer getCustomer()
+        {
+            Customer ct = new Customer();
+            ct.CustomerName = "abc";
+            ct.Address = "mvc";
+            return ct;
+        }
+    }
+}
